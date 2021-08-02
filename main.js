ec1c1539faa2f1130802bf184fa4a85e238adc63
Turarbek Yerassyl <erasiltyr@gmail.com>
2021-08-02 15:43:12 +0600

    Типы данных

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 // -----------------------------------------
+let money = 5000000;
+console.log('money: ', money);
+let income = 'Frilance';
+console.log('income: ', income);
+let addExpenses = 'Taxi, Internet';
+console.log('addExpenses: ', addExpenses.length);
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+let deposit = true;
+console.log('deposit: ', deposit);
+let mission = 10000000;
+let period = 12;
+console.log("Период равен " + period + " месяцев", "Цель заработать " + mission + " тенге");
